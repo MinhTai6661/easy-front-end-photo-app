8876aefcd66eb5879250cf5cc429c83a8e15cc79
MinhTai6661 <tai0167112684@gmail.com>
2022-06-16 22:19:25 +0700

    init and setup project

---
+++ src/features/pages/AddEdit/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+AddEditPage.propTypes = {};
+
+function AddEditPage(props) {
+    console.log('add edit page')
+    return <div>Photo Add Edit Page</div>;
+}
+
+export default AddEditPage;
